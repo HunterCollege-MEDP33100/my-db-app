5bbc4b03fea4a7640696d89cf6ff78278b0461ea
Dora Do <doradocodes@gmail.com>
2025-11-20 13:12:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL)
         console.log('Connected to MongoDB')
     })
     .catch((error) => {
-        console.error('Error connecting to MongoDB', error);
+        console.log('Error connecting to MongoDB', error);
     })
 
 // view engine setup
